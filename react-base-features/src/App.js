a00e0e013e9f00e1d28de47ca2d53f5ab8d8a7c9
David Jiang <djiang86@binghamton.edu>
2019-03-23 14:37:04 +0000

    create/export first component

--- react-base-features/src/App.js
+++ react-base-features/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I am a React app</h1>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+        <h1>Hi, I am a React app</h1>
+          <Person/>
+      </div>
+    );
 
-      return React.createElement('div', {
-          className: 'App'
-      }, React.createElement('h1', null, 'Does this work now?'));
+      // return React.createElement('div', {
+      //     className: 'App'
+      // }, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
