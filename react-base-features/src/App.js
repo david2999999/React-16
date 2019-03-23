97f13ac50331492c2f31dd65c5a33ff25620ff43
David Jiang <djiang86@binghamton.edu>
2019-03-23 15:02:05 +0000

    passing props into components

--- react-base-features/src/App.js
+++ react-base-features/src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I am a React app</h1>
-          <Person/>
-          <Person/>
-          <Person/>
+          <Person name="Max" age="28"/>
+          <Person name="Manu" age="29">My Hobbies Racing</Person>
+          <Person name="Stephanie" age="26"/>
       </div>
     );
 
