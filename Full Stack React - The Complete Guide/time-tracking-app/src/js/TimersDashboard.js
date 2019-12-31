3a4cfe96edba14e3159464f6e9a12c479453169a
david2999999 <djiang86@binghamton.edu>
2019-12-30 19:14:57 -0500

    creating timer dashboard

---
+++ Full Stack React - The Complete Guide/time-tracking-app/src/js/TimersDashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class TimersDashboard extends React.Component {
+    render() {
+        return (
+            <div className='ui three column centered grid'>
+                <div className='column'>
+                    <EditableTimerList/>
+                    <ToggleableTimerForm
+                        isOpen={true}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
