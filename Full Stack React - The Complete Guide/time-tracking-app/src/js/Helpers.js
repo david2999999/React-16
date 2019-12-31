8afbdc2d260d1042bb0b851a466a3aaf45c90449
david2999999 <djiang86@binghamton.edu>
2019-12-31 12:05:41 -0500

    adding state to editable timer

--- Full Stack React - The Complete Guide/time-tracking-app/src/js/Helpers.js
+++ Full Stack React - The Complete Guide/time-tracking-app/src/js/Helpers.js
@@ -7,7 +7,7 @@ export function renderElapsedString(elapsedTime) {
     let minutes = Math.floor(seconds / 60);
     seconds = seconds % 60;
 
-    return `${hours.toString().padStart(2, '0')}:
-            ${minutes.toString().padStart(2, '0')}:
-            ${seconds.toString().padStart(2, '0')}`;
+    return hours.toString().padStart(2, '0') + ':' +
+           minutes.toString().padStart(2, '0') + ':' +
+           seconds.toString().padStart(2, '0');
 }
