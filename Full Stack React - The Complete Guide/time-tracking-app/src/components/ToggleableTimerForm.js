af1c749b9b0a4061d28ba1d8f514761a5ae19612
david2999999 <djiang86@binghamton.edu>
2019-12-31 14:16:15 -0500

    propagating event from timer form to toggleable timer form

--- Full Stack React - The Complete Guide/time-tracking-app/src/components/ToggleableTimerForm.js
+++ Full Stack React - The Complete Guide/time-tracking-app/src/components/ToggleableTimerForm.js
@@ -12,10 +12,26 @@ export class ToggleableTimerForm extends React.Component {
         });
     };
 
+    handleFormClose = () => {
+        this.setState({
+            isOpen: false
+        });
+    };
+
+    handleFormSubmit = (timer) => {
+        this.props.onFormSubmit(timer);
+        this.setState({
+            isOpen: false
+        });
+    };
+
     render() {
         if (this.state.isOpen) {
             return (
-                <TimerForm/>
+                <TimerForm
+                    onFormSubmit={this.handleFormSubmit}
+                    onFormClose={this.handleFormClose}
+                />
             )
         } else {
             return (
