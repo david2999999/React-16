14e99cfd042a7c1f7a96dfbe2683f11f2cfb9e1c
david2999999 <djiang86@binghamton.edu>
2019-12-30 22:14:39 -0500

    creating static timer data

--- Full Stack React - The Complete Guide/time-tracking-app/src/components/TimersDashboard.js
+++ Full Stack React - The Complete Guide/time-tracking-app/src/components/TimersDashboard.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { EditableTimerList } from "./EditableTimerList";
 import { ToggleableTimerForm } from "./ToggleableTimerForm";
+import uuid from 'react-uuid';
 
 export class TimersDashboard extends React.Component {
+    state = {
+      timers: [
+          {
+              title: 'Practice Squat',
+              project: 'Gym Chores',
+              id: uuid(),
+              elapsed: 5456099,
+              runningSince: Date.now()
+          },
+          {
+              title: 'Bake Squash',
+              project: 'Kitchen Chores',
+              id: uuid(),
+              elapsed: 1273998,
+              runningSince: null
+          }
+      ]
+    };
+
     render() {
         return (
             <div className='ui three column centered grid'>
                 <div className='column'>
-                    <EditableTimerList/>
+                    <EditableTimerList
+                        timers={this.state.timers}
+                    />
                     <ToggleableTimerForm
                         isOpen={true}
                     />
