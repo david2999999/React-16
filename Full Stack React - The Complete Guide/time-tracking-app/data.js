87ac419bfdd1fe062e30521b35c19ea4d6dc3aea
david2999999 <djiang86@binghamton.edu>
2019-12-31 20:47:27 -0500

    setting up json server

---
+++ Full Stack React - The Complete Guide/time-tracking-app/data.js
@@ -0,0 +1,24 @@
+module.exports = function () {
+    return {
+        timers: [
+            {
+                title: 'Mow the lawn',
+                project: 'House Chores',
+                id: "13131868-cbc5-43fe-9eaa-ad7c60e3292a",
+                elapsed: 5456099
+            },
+            {
+                title: 'Clear Paper Jam',
+                project: 'Office Chores',
+                id: "16cf4e5e-a3bd-45b3-b77f-312ec4464adc",
+                elapsed: 1273998,
+            },
+            {
+                title: 'Ponder Origins of Universe',
+                project: 'Life Chores',
+                id: "16c21ase5e-a3bd-45b3-b77f-312ec4464adc",
+                elapsed: 11750,
+            }
+        ]
+    }
+};
