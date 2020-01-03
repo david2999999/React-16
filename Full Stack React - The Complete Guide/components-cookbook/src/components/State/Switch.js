bd1858adacac23645358c83e64994cee5e759dcc
david2999999 <djiang86@binghamton.edu>
2020-01-03 18:18:42 -0500

    component with state

---
+++ Full Stack React - The Complete Guide/components-cookbook/src/components/State/Switch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Switch extends React.Component {
+    state = {};
+
+    render() {
+        return (
+            <div><em>Template will be here</em></div>
+        )
+    }
+}
+
+module.exports = Switch;
