e49319ee304ee472073b122d1a09e5b6dee282d4
david2999999 <djiang86@binghamton.edu>
2020-01-03 18:21:43 -0500

    using the state in the component

--- Full Stack React - The Complete Guide/components-cookbook/src/components/State/Switch.js
+++ Full Stack React - The Complete Guide/components-cookbook/src/components/State/Switch.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+const CREDIT_CARD = 'Credit-Card';
+const BIT_COIN = 'Bitcoin';
+
 class Switch extends React.Component {
-    state = {};
+    state = {
+        payMethod: BIT_COIN
+    };
 
     render() {
         return (
-            <div><em>Template will be here</em></div>
+            <div className='switch'>
+                <div className='choice'>Credit Card</div>
+                <div className='choice'>BitCoin</div>
+                Pay with: {this.state.payMethod}
+            </div>
         )
     }
 }
