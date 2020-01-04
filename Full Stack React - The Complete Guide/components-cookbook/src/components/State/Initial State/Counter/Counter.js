0794f5c5232c19b39a3159bd98f7d616b03008f3
david2999999 <djiang86@binghamton.edu>
2020-01-03 21:07:05 -0500

    using prevState when the state change depends on the current state

--- Full Stack React - The Complete Guide/components-cookbook/src/components/State/Initial State/Counter/Counter.js
+++ Full Stack React - The Complete Guide/components-cookbook/src/components/State/Initial State/Counter/Counter.js
@@ -8,6 +8,41 @@ class Counter extends Component {
             value: this.props.initialValue
         };
     }
+
+    decrement = () => {
+      this.setState(prevState => {
+          return {
+              value: prevState.value - 1
+          };
+      });
+    };
+
+    increment = () => {
+        this.setState(prevState => {
+            return {
+                value: prevState.value + 1
+            }
+        });
+    };
+
+    render() {
+        return (
+            <div style={counterStyle} key="counter">
+                {this.state.value}
+                <p>
+                    <button onClick={this.increment}>+</button>
+                    <button onClick={this.decrement}>-</button>
+                </p>
+            </div>
+        );
+    }
 }
+
+const counterStyle = {
+    width: '50px',
+    textAlign: 'center',
+    backgroundColor: 'aliceblue',
+    padding: '10px'
+};
 
 export default Counter;
