ca7303c83999046d415b42abd429dc99db5675a1
david2999999 <djiang86@binghamton.edu>
2020-01-07 18:23:01 -0500

    removing messages from array with slice

--- Full Stack React - The Complete Guide/chat-app/src/store/reducer.js
+++ Full Stack React - The Complete Guide/chat-app/src/store/reducer.js
@@ -3,6 +3,13 @@ export function reducer(state, action) {
         return {
             messages: state.messages.concat(action.message)
         };
+    } else if (action.type === 'DELETE_MESSAGE') {
+         return  {
+             messages: [
+                 ...state.messages.slice(0, action.index),
+                 ...state.messages.slice(action.index + 1, state.messages.length)
+             ]
+         }
     } else {
         return state;
     }
