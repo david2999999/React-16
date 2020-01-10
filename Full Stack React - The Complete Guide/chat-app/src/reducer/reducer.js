32e5a4e0e98d0381416c66d223f561073cc971a8
david2999999 <djiang86@binghamton.edu>
2020-01-09 23:01:54 -0500

    using combinedReducers to separate property and reducer management

--- Full Stack React - The Complete Guide/chat-app/src/reducer/reducer.js
+++ Full Stack React - The Complete Guide/chat-app/src/reducer/reducer.js
@@ -1,9 +1,15 @@
 import { activeThreadIdReducer } from "./activeThreadIdReducer";
 import { threadsReducer } from "./threadsReducer";
+import { combineReducers } from "redux";
 
-export function reducer(state = {}, action) {
-    return {
-        activeThreadId: activeThreadIdReducer(state.activeThreadId, action),
-        threads: threadsReducer(state.threads, action)
-    }
-}
+// export function reducer(state = {}, action) {
+//     return {
+//         activeThreadId: activeThreadIdReducer(state.activeThreadId, action),
+//         threads: threadsReducer(state.threads, action)
+//     }
+// }
+
+export const reducer = combineReducers({
+    activeThreadId: activeThreadIdReducer,
+    threads: threadsReducer
+});
