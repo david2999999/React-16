7a994b4d357fa4214df7844f32d1371cff8c13fd
david2999999 <djiang86@binghamton.edu>
2020-01-09 21:42:17 -0500

    refactored thread reducer for finding thread index

--- Full Stack React - The Complete Guide/chat-app/src/reducer/threadsReducer.js
+++ Full Stack React - The Complete Guide/chat-app/src/reducer/threadsReducer.js
@@ -1,41 +1,41 @@
 import { messagesReducer } from "./messagesReducer";
 
 export function threadsReducer(state, action) {
-    if (action.type === 'ADD_MESSAGE') {
-        const threadIndex = state.findIndex((t) => {
-            return t.id === action.threadId;
-        });
+    switch (action.type) {
+        case 'ADD_MESSAGE':
+        case 'DELETE_MESSAGE': {
+            const threadIndex = findThreadIndex(state, action);
+            const oldThread = state[threadIndex];
+            const newThread = {
+                ...oldThread,
+                messages: messagesReducer(oldThread.messages, action)
+            };
 
-        const oldThread = state[threadIndex];
-        const newThread = {
-            ...oldThread,
-            messages: messagesReducer(oldThread.messages, action)
-        };
+            return [
+                ...state.slice(0, threadIndex),
+                newThread,
+                ...state.slice(threadIndex + 1, state.length)
+            ];
+        } default: {
+            return state;
+        }
+    }
+}
 
-        return [
-            ...state.slice(0, threadIndex),
-            newThread,
-            ...state.slice(threadIndex + 1, state.length)
-        ];
-    } else if (action.type === 'DELETE_MESSAGE') {
-         const threadIndex = state.findIndex((t) => (
-             t.messages.find((m) => (
-                 m.id === action.id
-             ))
-         ));
-         const oldThread = state[threadIndex];
-         const newThread = {
-             ...oldThread,
-             messages: messagesReducer(oldThread.messages, action)
-         };
-
-         return [
-            ...state.slice(0, threadIndex),
-            newThread,
-            ...state.slice(threadIndex + 1, state.length)
-         ];
-    } else {
-        return state;
+function findThreadIndex(threads, action) {
+    switch (action.type) {
+        case 'ADD_MESSAGE': {
+            return threads.findIndex(
+                (t) => t.id === action.threadId
+            )
+        }
+        case 'DELETE_MESSAGE': {
+            return threads.findIndex(
+                (t) => t.messages.find((m) => (
+                    m.id === action.id
+                ))
+            );
+        }
     }
 }
 
