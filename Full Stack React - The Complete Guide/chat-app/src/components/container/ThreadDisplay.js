620fb2efe82447934ef0689033ea2a1676dd40a9
david2999999 <djiang86@binghamton.edu>
2020-01-11 15:30:13 -0500

    using connect to create Thread display

--- Full Stack React - The Complete Guide/chat-app/src/components/container/ThreadDisplay.js
+++ Full Stack React - The Complete Guide/chat-app/src/components/container/ThreadDisplay.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { store } from "../../App";
 import {Thread} from "../Thread";
+import {connect} from 'react-redux';
 
 const mapStateToThreadProps = (state) => (
     {
@@ -36,37 +35,10 @@ const mergeThreadProps = (stateProps, dispatchProps) => (
     }
 );
 
-
-class ThreadDisplay extends React.Component {
-    componentDidMount() {
-        store.subscribe(() => this.forceUpdate());
-    }
-
-    render() {
-        const state = store.getState();
-        const activeThreadId = state.activeThreadId;
-        const threads = state.threads;
-        const activeThread = threads.find((t) => t.id === activeThreadId);
-
-        return (
-            <Thread thread={activeThread}
-                onMessageClick={(id) => (
-                    store.dispatch({
-                        type: 'DELETE_MESSAGE',
-                        id: id
-                    })
-                )}
-
-                onMessageSubmit={(text) => (
-                    store.dispatch({
-                        type: 'ADD_MESSAGE',
-                        text: text,
-                        threadId: activeThreadId
-                    })
-                )}
-            />
-        )
-    }
-}
+const ThreadDisplay = connect(
+    mapStateToThreadProps,
+    mapDispatchToThreadProps,
+    mergeThreadProps
+)(Thread);
 
 export default ThreadDisplay;
--- Full Stack React - The Complete Guide/chat-app/src/components/container/TreadTabs.js
+++ Full Stack React - The Complete Guide/chat-app/src/components/container/TreadTabs.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Tabs } from "../generic/Tabs";
 import { connect } from 'react-redux';
 
