d7a3fd762bad9966bee10d1431beb5095cf003cb
david2999999 <djiang86@binghamton.edu>
2020-01-12 00:05:16 -0500

    fetch people will app starts

--- Full Stack React - The Complete Guide/forms/src/App.js
+++ Full Stack React - The Complete Guide/forms/src/App.js
@@ -4,10 +4,15 @@ import {reducer} from "./reducer/reducer";
 import thunkMiddleware from 'redux-thunk';
 import ReduxForm from "./components/container/ReduxForm";
 import {Provider} from "react-redux/src";
+import {fetchPeople} from "./action/fetchRequest";
 
 const store = createStore(reducer, applyMiddleware(thunkMiddleware));
 
 class App extends React.Component {
+    componentWillMount() {
+        store.dispatch(fetchPeople());
+    }
+
     render() {
         return (
             <Provider store={store}>
