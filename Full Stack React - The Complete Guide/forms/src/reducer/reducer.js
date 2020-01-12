deaa619f74987cfe1f5021042824126d84c81435
david2999999 <djiang86@binghamton.edu>
2020-01-11 20:33:53 -0500

    reducer for save people requests

--- Full Stack React - The Complete Guide/forms/src/reducer/reducer.js
+++ Full Stack React - The Complete Guide/forms/src/reducer/reducer.js
@@ -1,4 +1,9 @@
-import {FETCH_PEOPLE_REQUEST, FETCH_PEOPLE_SUCCESS} from "../action/fetchRequest";
+import {
+    FETCH_PEOPLE_REQUEST,
+    FETCH_PEOPLE_SUCCESS, SAVE_PEOPLE_FAILURE,
+    SAVE_PEOPLE_REQUEST,
+    SAVE_PEOPLE_SUCCESS
+} from "../action/fetchRequest";
 
 const initialState = {
     people: [],
@@ -22,6 +27,27 @@ export function reducer(state = initialState, action) {
             return Object.assign({}, state, {
                 people: action.people,
                 isLoading: false
-            })
+            });
+        case SAVE_PEOPLE_REQUEST:
+            return Object.assign({}, state, {
+                saveStatus: 'SAVING'
+            });
+        case SAVE_PEOPLE_FAILURE:
+            return Object.assign({}, state, {
+               saveStatus: 'ERROR'
+            });
+        case SAVE_PEOPLE_SUCCESS:
+            return Object.assign({}, state, {
+                people: action.people,
+                person: {
+                    name: '',
+                    email: '',
+                    course: null,
+                    department: null
+                },
+                saveStatus: 'SUCCESS'
+            });
+        default:
+            return state;
     }
 }
