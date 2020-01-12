e4c64e5ba756b12f531aba00841484b20b0ceab6
david2999999 <djiang86@binghamton.edu>
2020-01-12 16:08:09 -0500

    creating a custom assert equal function

---
+++ Full Stack React - The Complete Guide/unit-testing/test/test-2.js
@@ -0,0 +1,21 @@
+import Modash from "../function/Modash";
+
+function assertEqual(description, actual, expected) {
+    if (actual === expected) {
+        console.log(`[PASS] ${description}`);
+    } else {
+        console.log(`[FAILED] ${description}`);
+        console.log(`\t actual: '${actual}'`);
+        console.log(`\t expected: '${expected}'`);
+    }
+}
+
+let actual;
+let expected;
+let string;
+
+string = 'there was one catch, and that was CATCH-22';
+actual = Modash.truncate(string, 19);
+expected = 'there was on catch...';
+
+assertEqual(`truncate(): truncates a string`, actual, expected);
