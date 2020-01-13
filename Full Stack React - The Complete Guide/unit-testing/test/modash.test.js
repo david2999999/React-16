89dc9b239d208a95fc9ae0595bed2d01ef397249
david2999999 <djiang86@binghamton.edu>
2020-01-13 17:29:37 -0500

    writing tests for capitalize

--- Full Stack React - The Complete Guide/unit-testing/test/modash.test.js
+++ Full Stack React - The Complete Guide/unit-testing/test/modash.test.js
@@ -16,4 +16,16 @@ describe('Modash', () => {
             ).toEqual(string);
         })
     });
+
+    describe('capitalize()', () => {
+        it ('capitalizes first letter, lowercases rest', () => {
+            const string = 'there was one catch, and that was CATCH-22';
+
+            expect(
+                Modash.capitalize(string)
+            ).toEqual(
+                'There was one catch, and that was catch-22'
+            )
+        });
+    });
 });
