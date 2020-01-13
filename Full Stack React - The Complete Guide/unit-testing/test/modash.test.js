1884da947f8d6b9e355329531756c760c3260fa8
david2999999 <djiang86@binghamton.edu>
2020-01-13 17:26:40 -0500

    using nested describe blocks

--- Full Stack React - The Complete Guide/unit-testing/test/modash.test.js
+++ Full Stack React - The Complete Guide/unit-testing/test/modash.test.js
@@ -1,10 +1,19 @@
 import Modash from "../function/Modash";
 
 describe('Modash', () => {
-   it('`truncate()`: truncates a string', () => {
-       const string = 'there was one catch, and that was CATCH-22';
-       expect(
-           Modash.truncate(string, 19)
-       ).toEqual('there was one catch...');
-   })
+    describe('`truncate()`', () => {
+        const string = 'there was one catch, and that was CATCH-22';
+
+        it ('truncates a string', () => {
+            expect(
+                Modash.truncate(string, 19)
+            ).toEqual('there was one catch...');
+        });
+
+        it ('no-ops if <= length', () => {
+            expect(
+                Modash.truncate(string, string.length)
+            ).toEqual(string);
+        })
+    });
 });
