6020103440d280e384b86ffa74fe7b74e1f7681d
david2999999 <djiang86@binghamton.edu>
2020-01-14 18:00:50 -0500

    simulating a form clear action

--- Full Stack React - The Complete Guide/enzyme-testing/src/tests/Behavior.test.js
+++ Full Stack React - The Complete Guide/enzyme-testing/src/tests/Behavior.test.js
@@ -30,6 +30,28 @@ describe('Form', () => {
             expect(
                 button.props().disabled
             ).toBe(false);
-        })
+        });
+
+        describe('and then clears the input', () => {
+           beforeEach(() => {
+               const input = wrapper.find('input').first();
+               input.simulate('change', {
+                   target: { value: '' }
+               })
+           });
+
+           it ('should disable `button`', () => {
+               const button = wrapper.find('button').first();
+               expect(
+                   button.props().disabled
+               ).toBe(true);
+           });
+
+           it ('should clear `item` value in state', () => {
+               expect(
+                   wrapper.state().item
+               ).toBe('');
+           })
+        });
     })
 });
