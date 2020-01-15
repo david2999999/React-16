99d48e9502ea9bcb82c3c1d54f55af7980276d21
david2999999 <djiang86@binghamton.edu>
2020-01-14 19:00:15 -0500

    writing boilerplate for test cases in food search

---
+++ Full Stack React - The Complete Guide/food-lookup-app/client/src/tests/FoodSearch.test.js
@@ -0,0 +1,35 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import FoodSearch from "../FoodSearch";
+
+describe('FoodSearch', () => {
+    describe('user populates search field', () => {
+        beforeEach(() => { // simulate user typing 'brocc' in input
+
+        });
+
+        describe('and API returns results', () => {
+            beforeEach(() => { // simulate API returning
+
+            });
+
+            describe('then user clicks food item', () => {
+                beforeEach(() => { // simulate user clicking food item
+
+                });
+
+                describe('then user types more', () => {
+                    beforeEach(() => { // simulate user typing x
+
+                    });
+
+                    describe('and API returns no results', () => {
+                        beforeEach(() => { // simulate API return no results
+
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
