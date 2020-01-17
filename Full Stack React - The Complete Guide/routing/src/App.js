5fd0b9dd839a690f47476b5de3b8e09e1bfb2f79
david2999999 <djiang86@binghamton.edu>
2020-01-16 22:34:09 -0500

    wrapping app with router, which contains history and location context

--- Full Stack React - The Complete Guide/routing/src/App.js
+++ Full Stack React - The Complete Guide/routing/src/App.js
@@ -3,37 +3,32 @@ import Atlantic from "./components/Atlantic";
 import Pacific from "./components/Pacific";
 import Route from "./router/Route";
 import Link from "./router/Link";
-import { history } from "./router/Link";
+import Router from "./router/Router";
 
-class App extends React.Component {
-    componentDidMount() {
-        history.listen(() => this.forceUpdate());
-    }
+const App = () => (
+    <Router>
+        <div className='ui text container'>
+            <h2 className='ui dividing header'>
+                Which body of water?
+            </h2>
+            <ul>
+                <li>
+                    <Link to='/atlantic'>
+                        <code>/atlantic</code>
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/pacific'>
+                        <code>/pacific</code>
+                    </Link>
+                </li>
+            </ul>
+            <hr/>
 
-    render() {
-    return (
-      <div className='ui text container'>
-        <h2 className='ui dividing header'>
-          Which body of water?
-        </h2>
-        <ul>
-          <li>
-              <Link to='/atlantic'>
-                  <code>/atlantic</code>
-              </Link>
-          </li>
-          <li>
-            <Link to='/pacific'>
-                <code>/pacific</code>
-            </Link>
-          </li>
-        </ul>
-        <hr/>
+            <Route path='/atlantic' component={Atlantic} />
+            <Route path='/pacific' component={Pacific} />
+        </div>
+    </Router>
+);
 
-        <Route path='/atlantic' component={Atlantic} />
-        <Route path='/pacific' component={Pacific} />
-      </div>
-    )
-  }
-}
 export default App;
