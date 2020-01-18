d56d03211ce9c48f0c35a72985ffcf7e375a98a8
david2999999 <djiang86@binghamton.edu>
2020-01-17 23:32:11 -0500

    initial template of music app

--- Full Stack React - The Complete Guide/music-app/client/src/App.js
+++ Full Stack React - The Complete Guide/music-app/client/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import TopBar from './components/TopBar';
+import AlbumsContainer from './components/AlbumsContainer';
+
+import './styles/App.css';
+
+const App = () => (
+  <div className='ui grid'>
+    <TopBar />
+    <div className='spacer row' />
+    <div className='row'>
+      <AlbumsContainer />
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
