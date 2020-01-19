1f62ed76616950842587660cc1ce54bda0b9ab0c
David Jiang <djiang86@Binghamton.edu>
2020-01-19 08:11:04 -0500

    higher order component private route to redirect users if they are no authenticated

--- Full Stack React - The Complete Guide/music-app/client/src/App.js
+++ Full Stack React - The Complete Guide/music-app/client/src/App.js
@@ -5,13 +5,14 @@ import AlbumsContainer from './components/container/AlbumsContainer';
 import {Redirect, Route} from 'react-router-dom';
 import Login from "./components/container/Login";
 import Logout from "./components/button/Logout";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => (
   <div className='ui grid'>
     <TopBar />
     <div className='spacer row' />
     <div className='row'>
-        <Route path='/albums' component={AlbumsContainer}/>
+        <PrivateRoute path='/albums' component={AlbumsContainer}/>
         <Route path='/login' component={Login}/>
         <Route path='/logout' component={Logout}/>
         <Route exact path='/' render={() => (
