067cc64928dad46e6c139e28bc67bf4e6a394539
David Jiang <djiang86@Binghamton.edu>
2020-01-21 18:56:43 -0500

    schema for graphql

--- Full Stack React - The Complete Guide/graphql-server/server.js
+++ Full Stack React - The Complete Guide/graphql-server/server.js
@@ -1,10 +1,27 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
 
 const app = express();
 
-app.use('/graphql', (req, res) => {
-    res.send({data: true});
+const RootQuery = new GraphQLObjectType({
+   name: 'RootQuery',
+   description: 'The root Query',
+   fields: {
+       viewer: {
+           type: GraphQLString,
+           resolve() {
+               return 'viewer!';
+           }
+       }
+   }
 });
+
+const Schema = new GraphQLSchema({
+    query: RootQuery
+});
+
+app.use('/graphql', graphqlHTTP({ schema: Schema }));
 
 app.listen(3000, () => {
     console.log({ running: true });
