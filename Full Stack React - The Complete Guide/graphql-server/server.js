19b7c50d53ab173c952931e72fa3a9438b9ee330
David Jiang <djiang86@Binghamton.edu>
2020-01-21 18:35:38 -0500

    initial server setup with express

---
+++ Full Stack React - The Complete Guide/graphql-server/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.use('/graphql', (req, res) => {
+    res.send({data: true});
+});
+
+app.listen(3000, () => {
+    console.log({ running: true });
+});
+
