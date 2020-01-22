1f22272be9101f59f019dea66cd4a044c4f9f8e8
David Jiang <djiang86@Binghamton.edu>
2020-01-21 21:17:17 -0500

    creating a root mutation method

--- Full Stack React - The Complete Guide/graphql-server/server.js
+++ Full Stack React - The Complete Guide/graphql-server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString,
+    GraphQLNonNull, GraphQLID } from 'graphql';
 
 const app = express();
 
@@ -21,7 +22,32 @@ const Schema = new GraphQLSchema({
     query: RootQuery
 });
 
-app.use('/graphql', graphqlHTTP({ schema: Schema }));
+app.use('/graphql', graphqlHTTP({ schema: Schema, graphiql: true }));
+
+let inMemoryStore = {};
+
+const RootMutation = new GraphQLObjectType({
+   name: 'RootMutation',
+   description: 'The root mutation',
+   fields: {
+       setNode: {
+           type: GraphQLString,
+           args: {
+               id: {
+                   type: new GraphQLNonNull(GraphQLID)
+               },
+               value: {
+                   type: new GraphQLNonNull(GraphQLString)
+               }
+           },
+           resolve(source, args) {
+               inMemoryStore[args.key] = args.value;
+               return inMemoryStore[args.key];
+           }
+       }
+   }
+});
+
 
 app.listen(3000, () => {
     console.log({ running: true });
