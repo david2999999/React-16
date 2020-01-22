9159cb78b109a7e1d3870a59b90f7ec9d70b4240
David Jiang <djiang86@Binghamton.edu>
2020-01-22 17:48:19 -0500

    posts table

--- Full Stack React - The Complete Guide/Facebook-Clone/src/table.js
+++ Full Stack React - The Complete Guide/Facebook-Clone/src/table.js
@@ -31,3 +31,24 @@ export const usersFriends = sql.define({
     }]
 });
 
+export const posts = sql.define({
+    name: 'posts',
+    columns: [{
+        name: 'id',
+        dataType: 'INTEGER',
+        primaryKey: true
+    }, {
+        name: 'user_id',
+        dataType: 'int'
+    }, {
+        name: 'body',
+        dataType: 'text'
+    }, {
+        name: 'level',
+        dataType: 'text'
+    }, {
+        name: 'created_at',
+        dataType: 'datetime'
+    }]
+});
+
