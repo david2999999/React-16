ca05df7ed7a214eec7d9f0ce6fd1c1016e6785d1
David Jiang <djiang86@Binghamton.edu>
2020-01-30 18:56:48 -0500

    creating html elements with react.createElement

--- React - The Complete Guide - Udemy Course/react-basics/src/App.js
+++ React - The Complete Guide - Udemy Course/react-basics/src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return React.createElement('div', {className:'App'},
+            React.createElement('h1', null, 'Does this work now?'));
+    }
 }
 
 export default App;
