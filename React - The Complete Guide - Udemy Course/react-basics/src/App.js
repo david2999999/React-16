04bbf11ea14a88717ce00309d44c630cf5ca5cec
David Jiang <djiang86@Binghamton.edu>
2020-01-30 20:30:18 -0500

    using state property in component

--- React - The Complete Guide - Udemy Course/react-basics/src/App.js
+++ React - The Complete Guide - Udemy Course/react-basics/src/App.js
@@ -3,15 +3,24 @@ import Person from "./components/Person/Person";
 import './App.css';
 
 class App extends Component {
+    state = {
+        persons: [
+            { name: 'David', age: 28 },
+            { name: 'Manu', age: 29 },
+            { name: 'Steph', age: 32 }
+        ]
+    };
+
     render() {
         return (
             <div className='App'>
                 <h1>Hi, I'm React App.</h1>
-                <Person name="David" age="24"/>
-                <Person name="Manu" age="29">
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
                     My Hobbies: Coding
                 </Person>
-                <Person name="Steph" age="32"/>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
             </div>
         )
     }
