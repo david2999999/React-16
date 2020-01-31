a9da3455d8682519e66c40d8dd3fa31763a50680
David Jiang <djiang86@Binghamton.edu>
2020-01-30 19:47:33 -0500

    creating a functional component

--- React - The Complete Guide - Udemy Course/react-basics/src/App.js
+++ React - The Complete Guide - Udemy Course/react-basics/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import Person from "./components/Person/Person";
 import './App.css';
 
 class App extends Component {
     render() {
-        return React.createElement('div', {className:'App'},
-            React.createElement('h1', null, 'Does this work now?'));
+        return (
+            <div className='App'>
+                <h1>Hi, I'm React App.</h1>
+                <Person/>
+            </div>
+        )
     }
+    // render() {
+    //     return React.createElement('div', {className:'App'},
+    //         React.createElement('h1', null, 'Does this work now?'));
+    // }
 }
 
 export default App;
