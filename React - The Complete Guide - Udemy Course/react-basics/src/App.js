e2da03ffea37393ac41f88508d1f899dfd75f42f
David Jiang <djiang86@Binghamton.edu>
2020-01-31 20:06:32 -0500

    passing arguments into event handlers

--- React - The Complete Guide - Udemy Course/react-basics/src/App.js
+++ React - The Complete Guide - Udemy Course/react-basics/src/App.js
@@ -12,10 +12,10 @@ class App extends Component {
         otherState: 'some other value'
     };
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         this.setState({
             persons: [
-                { name: 'Davido', age: 28 },
+                { name: newName, age: 28 },
                 { name: 'Manu', age: 29 },
                 { name: 'Steph', age: 27 }
             ]
@@ -26,11 +26,11 @@ class App extends Component {
         return (
             <div className='App'>
                 <h1>Hi, I'm React App.</h1>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <button onClick={() => this.switchNameHandler('David Click')}>Switch Name</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
                 <Person name={this.state.persons[1].name}
                         age={this.state.persons[1].age}
-                        click={this.switchNameHandler}>
+                        click={this.switchNameHandler.bind(this, 'David Click 2')}>
                     My Hobbies: Coding
                 </Person>
                 <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
