9dd0df757c4967f46d1fb8dbb0cdc6b492ee0272
David Jiang <djiang86@Binghamton.edu>
2020-02-01 14:52:09 -0500

    exporting only one type with export default

---
+++ Learning React Functional Web Development PDF/Chapter 2 - Emerging Javascript/ES6 Modules/export-single.js
@@ -0,0 +1,7 @@
+// const freel = new Expedition("Mt. Freel", 2, ["water", "snack"]);
+
+// export default freel;
+
+// export default can be used in place of export when you wish to export only one
+// type. Again, both export and export default can be used on any JavaScript type:
+// primitives, objects, arrays, and functions
