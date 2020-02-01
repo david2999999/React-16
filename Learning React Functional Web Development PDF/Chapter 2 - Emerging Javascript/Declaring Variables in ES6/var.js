61f0a75967e895474e3d2729892a3ea493c1c735
David Jiang <djiang86@Binghamton.edu>
2020-02-01 12:52:33 -0500

    keyword var does not block off a variable's scope in for loop

--- Learning React Functional Web Development PDF/Chapter 2 - Emerging Javascript/Declaring Variables in ES6/var.js
+++ Learning React Functional Web Development PDF/Chapter 2 - Emerging Javascript/Declaring Variables in ES6/var.js
@@ -10,3 +10,20 @@ if (topic) {
 console.log('global', topic); // global React
 
 // The topic variable inside the if block resets the value of topic.
+
+var div, container = document.getElementById('container');
+
+for (var i = 0; i < 5; i++) {
+    div = document.createElement('div');
+    div.onclick = function () {
+        alert('This is box #' + i);
+    };
+
+    container.appendChild(div);
+}
+
+// In this loop, we create five divs to appear within a container. Each div is assigned an
+// onclick handler that creates an alert box to display the index. Declaring i in the for
+// loop creates a global variable named i, and then iterates it until its value reaches 5.
+// When you click on any of these boxes, the alert says that i is equal to 5 for all divs,
+// because the current value for the global i is 5
