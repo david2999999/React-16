e01e4d875bc9bdb1f6d8d61e73de9e9b07362903
David Jiang <djiang86@Binghamton.edu>
2020-02-01 17:47:11 -0500

    outputting list of element using the map function

--- React - The Complete Guide - Udemy Course/react-list-and-conditionals/src/App.js
+++ React - The Complete Guide - Udemy Course/react-list-and-conditionals/src/App.js
@@ -52,14 +52,11 @@ class App extends Component {
 
         let persons = this.state.showPersons ? (
             <div>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-                <Person name={this.state.persons[1].name}
-                        age={this.state.persons[1].age}
-                        click={this.switchNameHandler.bind(this, 'David Click 2')}
-                        changed={this.nameChangeHandler}>
-                    My Hobbies: Coding
-                </Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                {
+                    this.state.persons.map((person) => (
+                        <Person name={person.name} age={person.age}/>
+                    ))
+                }
             </div>
         ) : null;
 
@@ -74,10 +71,6 @@ class App extends Component {
             </div>
         )
     }
-    // render() {
-    //     return React.createElement('div', {className:'App'},
-    //         React.createElement('h1', null, 'Does this work now?'));
-    // }
 }
 
 export default App;
