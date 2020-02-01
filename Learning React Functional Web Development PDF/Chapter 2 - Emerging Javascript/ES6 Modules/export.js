ee665bbc8d68798d3b45ba977d41fdf9f484cf59
David Jiang <djiang86@Binghamton.edu>
2020-02-01 14:50:27 -0500

    exporting modules in es6

---
+++ Learning React Functional Web Development PDF/Chapter 2 - Emerging Javascript/ES6 Modules/export.js
@@ -0,0 +1,7 @@
+// export can be used to export any JavaScript type that will be consumed in another
+// module. In this example the print function and log function are being exported.
+
+// export const print(message) => log(message, new Date());
+
+// export const log(message, timestamp) =>
+//     console.log(`${timestamp.toString()}: ${message}`};
