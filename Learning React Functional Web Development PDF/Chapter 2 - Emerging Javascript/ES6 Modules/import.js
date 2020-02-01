63a1f9240c02f11ef6d4b55522a5ae971503bb27
David Jiang <djiang86@Binghamton.edu>
2020-02-01 14:55:46 -0500

    importing other javascript files with import statement

---
+++ Learning React Functional Web Development PDF/Chapter 2 - Emerging Javascript/ES6 Modules/import.js
@@ -0,0 +1,10 @@
+// Modules can be consumed in other JavaScript files using the import statement. Modules
+// with multiple exports can take advantage of object destructuring. Modules that
+// use export default are imported into a single variable:
+
+// import { print, log } from './text-helpers';
+// import freel from './mt-freel';
+
+// print('printing a message');
+// log('logging a message');
+// freel.print();
