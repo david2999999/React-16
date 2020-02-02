5f444725b3e17b1b56bd5bf5c2065289aa52a871
David Jiang <djiang86@Binghamton.edu>
2020-02-02 12:11:37 -0500

    declarative way to handle data loaded from an API

--- Learning React Functional Web Development PDF/Chapter 3 - Functional Programming with JavaScript/Imperative Verus Declarative/Declarative.js
+++ Learning React Functional Web Development PDF/Chapter 3 - Functional Programming with JavaScript/Imperative Verus Declarative/Declarative.js
@@ -9,3 +9,25 @@ console.log(urlFriendly);
 // spaces are dealt with are abstracted away inside the replace function. In a declarative
 // program, the syntax itself describes what should happen and the details of how things
 // happen are abstracted away.
+
+
+const loadAndMapMembers = compose(
+    combineWith(sessionStorage, "members"),
+    save(sessionStorage, "members"),
+    scopeMembers(window),
+    logMemberInfoToConsole,
+    logFieldsToConsole("name.first"),
+    countMembersBy("location.state"),
+    prepStatesForMapping,
+    save(sessionStorage, "map"),
+    renderUSMap
+);
+getFakeMembers(100).then(loadAndMapMembers);
+
+// The declarative approach is more readable and, thus, easier to reason about. The
+// details of how each of these functions is implemented are abstracted away. Those tiny
+// functions are named well and combined in a way that describes how member data
+// goes from being loaded to being saved and printed on a map, and this approach does
+// not require many comments. Essentially, declarative programming produces applications
+// that are easier to reason about, and when it is easier to reason about an application,
+// that application is easier to scale
