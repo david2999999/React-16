53166ed975b382b14f10da31e063fac385f474ac
David Jiang <djiang86@Binghamton.edu>
2020-02-02 13:09:41 -0500

    react's pure dom construction by creating a DOM and does not cause side effect to the surrounding, and allowing other elements to change the DOM

--- Learning React Functional Web Development PDF/Chapter 3 - Functional Programming with JavaScript/Functional Concepts/Pure Functions/functions/pure-function.js
+++ Learning React Functional Web Development PDF/Chapter 3 - Functional Programming with JavaScript/Functional Concepts/Pure Functions/functions/pure-function.js
@@ -20,10 +20,3 @@ console.log( frederick );
 // Finally, this version of selfEducate is a pure function. It computes a value based on
 // the argument that was sent to it: the person. It returns a new person object without
 // mutating the argument sent to it and therefore has no side effects.
-
-// Pure functions are naturally testable. They do not change anything
-// about their environment or “world,” and therefore do not require a
-// complicated test setup or teardown. Everything a pure function
-// needs to operate it accesses via arguments. When testing a pure
-// function, you control the arguments, and thus you can estimate the
-// outcome
