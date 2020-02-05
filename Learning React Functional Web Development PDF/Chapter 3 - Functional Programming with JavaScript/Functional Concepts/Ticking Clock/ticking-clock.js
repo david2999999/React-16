038079297f1b5d992f1922f486fded298c4a8fd8
David Jiang <djiang86@Binghamton.edu>
2020-02-04 21:18:28 -0500

    declarative way of programming

--- Learning React Functional Web Development PDF/Chapter 3 - Functional Programming with JavaScript/Functional Concepts/Ticking Clock/ticking-clock.js
+++ Learning React Functional Web Development PDF/Chapter 3 - Functional Programming with JavaScript/Functional Concepts/Ticking Clock/ticking-clock.js
@@ -40,29 +40,3 @@ function getClockTime() {
             + time.seconds + " "
             + time.ampm;
 }
-
-const oneSecond = () => 1000;
-const getCurrentTime = () => new Date();
-const clear = () => console.clear();
-const log = message => console.log(message);
-
-const serializeClockTime = date =>
-    ({
-        hours: date.getHours,
-        minutes: date.getMinutes,
-        seconds: date.getSeconds
-    });
-
-const civilianHours = clockTime =>
-    ({
-        ...clockTime,
-        hours: (clockTime.hours > 12) ?
-            clockTime.hours - 12 :
-            clockTime.hours
-    });
-
-const appendAMPM = clockTime =>
-    ({
-        ...clockTime,
-        ampm: (clockTime.hours >= 12) ? "PM" : "AM"
-    });
