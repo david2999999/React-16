4fee2e4795b105633dbf5f3b535557801930a30a
David Jiang <djiang86@Binghamton.edu>
2020-02-04 21:01:54 -0500

    functions to transform the date

--- Learning React Functional Web Development PDF/Chapter 3 - Functional Programming with JavaScript/Ticking Clock/ticking-clock.js
+++ Learning React Functional Web Development PDF/Chapter 3 - Functional Programming with JavaScript/Ticking Clock/ticking-clock.js
@@ -45,3 +45,24 @@ const oneSecond = () => 1000;
 const getCurrentTime = () => new Date();
 const clear = () => console.clear();
 const log = message => console.log(message);
+
+const serializeClockTime = date =>
+    ({
+        hours: date.getHours,
+        minutes: date.getMinutes,
+        seconds: date.getSeconds
+    });
+
+const civilianHours = clockTime =>
+    ({
+        ...clockTime,
+        hours: (clockTime.hours > 12) ?
+            clockTime.hours - 12 :
+            clockTime.hours
+    });
+
+const appendAMPM = clockTime =>
+    ({
+        ...clockTime,
+        ampm: (clockTime.hours >= 12) ? "PM" : "AM"
+    });
