e2708d8cc2178f28c2b1b8116b6a563193274fd0
David Jiang <djiang86@Binghamton.edu>
2020-02-05 22:49:13 -0500

    higher order component example

--- React - The Complete Guide - Udemy Course/react-component-deep-dive/src/containers/App.js
+++ React - The Complete Guide - Udemy Course/react-component-deep-dive/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Persons from '../components/Persons/Persons';
 import Cockpit from "../components/Cockpit/Cockpit";
 import './App.css';
+import WithClass from "../hoc/WithClass";
 
 class App extends Component {
     constructor(props) {
@@ -84,7 +85,7 @@ class App extends Component {
         }
 
         return (
-            <div className='App'>
+            <WithClass classes='App'>
                 <button onClick={() => {
                     this.setState({showCockpit: false})}
                 }>
@@ -96,7 +97,7 @@ class App extends Component {
                              personsLength={this.state.persons.length}
                              clicked={this.togglePersonsHandler}/> : null}
                 { persons }
-            </div>
+            </WithClass>
         )
     }
 }
