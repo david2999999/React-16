c73a76d34c6539e8599a92949b08de2498992297
David Jiang <djiang86@Binghamton.edu>
2020-02-06 21:22:42 -0500

    prop types check

--- React - The Complete Guide - Udemy Course/react-component-deep-dive/src/components/Persons/Person/Person.js
+++ React - The Complete Guide - Udemy Course/react-component-deep-dive/src/components/Persons/Person/Person.js
@@ -1,25 +1,25 @@
 import React, {Component} from 'react';
-import styled from 'styled-components';
+import PropTypes from 'prop-types';
+// import styled from 'styled-components';
 import withClass from "../../../hoc/withClass";
 import './Person.css';
 
-const StyleDiv = styled.div` 
-    width: 60%;
-    margin: 10px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media(min-width: 500px) {
-        width: 450px;
-    }
-`;
+// const StyleDiv = styled.div`
+//     width: 60%;
+//     margin: 10px auto;
+//     border: 1px solid #eee;
+//     box-shadow: 0 2px 3px #ccc;
+//     padding: 16px;
+//     text-align: center;
+//
+//     @media(min-width: 500px) {
+//         width: 450px;
+//     }
+// `;
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering....');
-
         return (
             <React.Fragment>
                 <p onClick={this.props.click}>
@@ -33,5 +33,12 @@ class Person extends Component {
         )
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default withClass(Person, 'Person');
