aaa01e8c00d970fee113c433b9d3b7a4c84c6c94
David Jiang <djiang86@Binghamton.edu>
2020-02-06 21:41:50 -0500

    using refs

--- React - The Complete Guide - Udemy Course/react-component-deep-dive/src/components/Persons/Person/Person.js
+++ React - The Complete Guide - Udemy Course/react-component-deep-dive/src/components/Persons/Person/Person.js
@@ -18,6 +18,16 @@ import './Person.css';
 // `;
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        //this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering....');
         return (
@@ -27,6 +37,8 @@ class Person extends Component {
                 </p>
                 <p>{ this.props.children }</p>
                 <input type="text"
+                       //ref={(inputEl) => {this.inputElement = inputEl}}
+                       ref={this.inputElementRef}
                        onChange={this.props.changed}
                        value={this.props.name}/>
             </React.Fragment>
