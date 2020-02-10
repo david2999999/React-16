03fc35f8bd4ba0072cf654bc74519c8123d6ad3c
David Jiang <djiang86@Binghamton.edu>
2020-02-09 21:05:15 -0500

    burger builder container component

--- React - The Complete Guide - Udemy Course/burger-builder/src/App.js
+++ React - The Complete Guide - Udemy Course/burger-builder/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Layout from "./components/Layouts/Layouts";
+import BurgerBuilder from "./containers/BugerBuilder/BurgerBuilder";
 
 function App() {
   return (
     <div>
-      <Layout/>
+        <Layout>
+            <BurgerBuilder/>
+        </Layout>
     </div>
   );
 }
