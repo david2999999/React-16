f26b45b3b2d52a1b05ec8ec543f628b58eb9c0b4
David Jiang <djiang86@binghamton.edu>
2022-07-28 10:12:20 -0400

    the concept of composition

--- React - The Complete Guide (incl Hooks, React Router, Redux)/react-complete-guide/src/components/Expenses.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/react-complete-guide/src/components/Expenses.js
@@ -1,15 +1,16 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import Card from "./Card";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
       ></ExpenseItem>
-    </div>
+    </Card>
   );
 }
 
